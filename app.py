c9df73f626c2ac5b007e06fda29906867d8920d3
Faheena-Thesni <faheenathes@gmail.com>
2026-02-12 13:58:57 +0530

    updated carbon dashboard and templates

--- app.py
+++ app.py
@@ -1,12 +1,29 @@
-# Flask web app
-
-from flask import Flask, render_template
-import os
+from flask import Flask, render_template, jsonify
+import requests
 
 app = Flask(__name__)
 
 @app.route("/")
 def dashboard():
-    return render_template("dashboard.html")
+    return render_template("dashboard.html", title="Dashboard")
 
-# DO NOT use app.run() on Render
+@app.route("/about")
+def about():
+    return render_template("about.html", title="About")
+
+@app.route("/api/emissions")
+def get_emissions():
+    BASE_URL = "https://api.climatetrace.org/v7/sources/emissions"
+
+    params = {
+        "year": 2021,
+        "sectors": "transportation",
+        "subsectors": "road-transportation",
+        "gas": "co2e_100yr"
+    }
+
+    response = requests.get(BASE_URL, params=params)
+    return response.json()
+
+if __name__ == "__main__":
+    app.run()
